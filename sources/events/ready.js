5a7031a7af62af67a04450d09d7b5d7b11742eab
VanillaNk2006 <77216086+VanillaNk2006@users.noreply.github.com>
2022-12-22 11:32:01 -0600

    elementos añadidos

--- sources/events/ready.js
+++ sources/events/ready.js
@@ -1,9 +1,15 @@
 // importaciones
 
+require('dotenv').config()
 const discord = require('discord.js')
+const models = require('maplebot_models')
 const package_json = require('../../package.json')
 const configs = require('../utils/exports.js')
 const { SlashManager } = require('../utils/slashCommandManager')
+const Webhook = new discord.WebhookClient({
+    id: process.env['eventsId'],
+    token: process.env['eventsToken']
+})
 
 // exportacion de arreglo con nombre y funcion del evento
 
@@ -22,6 +28,41 @@ exports.event = {
             }]
         })
         SlashManager(client);
-    	console.info(`El cliente se ha iniciado correctamente como: ${client.user.username}`)
+        console.info(`El cliente se ha iniciado correctamente como: ${client.user.username}`)
+        try {
+            const promises = [
+                client.shard.fetchClientValues('guilds.cache.size'),
+                client.shard.broadcastEval(c => c.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0))
+            ]
+            let results = await Promise.all(promises);
+            let totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
+            let totalMembers = results[1].reduce((acc, memberCount) => acc + memberCount, 0);
+            await Webhook.send({
+                embeds: [{
+                    author: {
+                        name: client.user.username,
+                        icon_url: client.user.avatarURL({ extension: 'png', size: 512 })
+                    },
+                    description: 'Me he vuelo a iniciar correctamente con... <:006:1012749025398759425>',
+                    title: 'Reinicio de la bot',
+                    color: configs.randomColor,
+                    timestamp: new Date(),
+                    fields: [{
+                        name: 'Estadisticas',
+                        value: `${models.menus.stats({
+                            servers: totalGuilds,
+                            interactions: client.application?.commands.cache.size,
+                            commands: client.comandos.size,
+                            users: totalMembers,
+                            votos: null,
+                            shards: null
+                        })}`
+                    }]
+                }]
+            })
+            await client.channels.cache.get('863902846294163490').setName(`⌠🔔 servers⌡: ${totalGuilds}`)
+        } catch (error) {
+            console.error(error)
+        }
     }
 }
