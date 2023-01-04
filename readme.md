22f0f2b0072a87f62ea175e038be468073adc02e
CacaoNk0027 <jbzca@outlook.es>
2023-01-04 12:45:18 -0600

    elementos añadidos

--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 <div align="center">
   <p><a href="https://discord.gg/PKGhvUKaQN"><img src="https://media.discordapp.net/attachments/809089744574611507/855990674046582794/1624150826097.png?width=322&height=461" width="70" alt="cacao_nekoapi" /></a></p>
   <p><b>Maple bot</b></p>
-  <p>por: ! Piccolo Scherzo#7836</p>
+  <p>por: ¡ Kuro SF#3915</p>
 </div>
 
 hey que tal? soy ¡ Kuro SF#3915, el desarollador oficial de maple bot, estoy desarrollando a Maple bot y a la vez la estoy actualizando para su version 2.3.0!, puedes invitar a la bot por medio de un enlace que estara
---
+++ sources/commands/informacion/uptime.js
@@ -0,0 +1,65 @@
+const discord = require('discord.js')
+const models = require('maplebot_models')
+const moment = require('moment')
+const config = require('../../utils/exports')
+const ms = require('ms')
+
+require('moment-duration-format')(moment)
+
+/**
+ * @param {discord.Client} client 
+ * @param {discord.Message} message 
+ * @param {import('../../typings').args} args 
+ */
+exports.text = async (client, message, args) => {
+    try {
+        await message.reply({
+            embeds: [{
+                title: '<:001:1012749015969968138> | Uptime',
+                description: `Este es el timepo de actividad que he tenido desde que me inicie por ultima vez`,
+                color: 0xffffff,
+                fields: [{
+                    name: 'Tiempo de actividad',
+                    value: `\`\`\`\n${moment.duration(client.uptime).format('W [Semanas] D [Días], H [Horas], m [Minutos], s [Segundos]')}\n\`\`\``
+                }],
+                author: {
+                    name: client.user?.username,
+                    icon_url: client.user?.avatarURL()
+                }
+            }]
+        })
+    } catch (error) {
+        await models.utils.error(message, error)
+    }
+}
+
+/**
+ * @param {discord.Client} client
+ * @param {discord.CommandInteraction} interaction
+ */
+exports.slash = async (client, interaction) => {
+    try {
+        
+    } catch (error) {
+        await config.interactionErrorMsg(interaction, error);
+    }
+}
+
+exports.help = {
+    name: 'uptime',
+    alias: [],
+    id: '019',
+    description: 'Ve cuando tiempo he estado encendida',
+    category: 'informacion',
+    options: [],
+    permissions: {
+        user: [],
+        bot: ['SendMessages', 'EmbedLinks']
+    },
+    status: {
+        code: 1,
+        reason: null
+    },
+    isNsfw: false,
+    cooldown: (ms('3s') / 1000)
+}
