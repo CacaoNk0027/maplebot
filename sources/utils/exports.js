71058a5342c71f57360bf00d2b2bab55400669b1
CacaoNk0027 <jbzca@outlook.es>
2023-06-22 02:25:43 -0600

    elementos añadidos

--- sources/utils/exports.js
+++ sources/utils/exports.js
@@ -5,25 +5,8 @@ const { Guild, ButtonInteraction, Message, AttachmentBuilder, Collection, Comman
 
 let defaultOptions = configs.menu_options
 let guildDefaultOptions = configs.guild_options
-
-let one_ = {
-    "label": "Nsfw",
-    "emoji": "853975964123398144",
-    "description": "comandos solo para nsfw",
-    "value": "11"
-}
-let two_ = {
-    "label": "Rolplay Nsfw",
-    "emoji": "869814289727381575",
-    "description": "comandos para rolplay nsfw",
-    "value": "12"
-}
-let g_one_ = {
-    "label": "Comunidad",
-    "emoji": "888230718780633108",
-    "description": "Infomacion sobre el servidor ( Comunidad )",
-    "value": "3"
-}
+let optionsWnsfw = configs.menu_options_wnsfw
+let commGuildOptions = configs.guild_options_wcomm
 
 exports.presences = async (client) => {
     var json_string = JSON.stringify(configs.presences).replace('$developer', (await client.application?.fetch()).owner.username).replace('$guilds', (client.guilds.cache.size)).replace('$commands', client.comandos.size)
@@ -34,13 +17,14 @@ exports.blacklist = configs.blacklist;
 exports.permissions = configs.permissions;
 
 exports.menuOptions = (channel) => {
-    if (!channel.nsfw) return defaultOptions; else {
-        defaultOptions.push(one_);
-        defaultOptions.push(two_);
-        return defaultOptions;
-    }
+    return channel.nsfw ? optionsWnsfw: defaultOptions;
 }
-
+/**
+ * @param {Guild} guild 
+ */
+exports.guildMenuOptions = (guild) => {
+    return guild.features.includes('COMMUNITY') ? commGuildOptions: guildDefaultOptions;
+}
 exports.interactionErrorMsg = async (interaction, error) => {
     console.error(error)
     await interaction.channel.send({
@@ -264,15 +248,7 @@ exports.fetchChannel = async (options) => {
     }
 }
 
-/**
- * @param {Guild} guild 
- */
-exports.guildMenuOptions = (guild) => {
-    if (!guild.features.includes('COMMUNITY')) return guildDefaultOptions; else {
-        guildDefaultOptions.push(g_one_);
-        return guildDefaultOptions;
-    }
-}
+
 
 exports.newColorImage = async (hexColor) => {
     if (hexColor == null) return {
