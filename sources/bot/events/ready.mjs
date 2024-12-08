ed5f9e0b24acdb420fa88cd2149b6dfd1f4d0c8e
CacaoNk0027 <jbzca@outlook.es>
2024-12-08 14:03:10 -0600

    archivos cambiados

--- sources/bot/events/ready.mjs
+++ sources/bot/events/ready.mjs
@@ -1,5 +1,11 @@
 import * as discord from 'discord.js'
 import * as configs from '../config/config.mjs'
+import sdk from '@top-gg/sdk'
+
+const topApi = new sdk.Api(process.env.TOPGG_TOKEN)
+const webhook = new discord.WebhookClient({
+    url: process.env.EVENTS_WEBHOOK
+})
 
 const name = 'ready'
 
@@ -25,6 +31,7 @@ async function main(client) {
  */
 async function stats(client) {
     let totalGuilds, totalMembers
+    let votes
     try {
         let [ guildCount, memberCount ] = await Promise.all([
             client.shard.fetchClientValues('guilds.cache.size'),
@@ -33,9 +40,26 @@ async function stats(client) {
 
         totalGuilds = guildCount.reduce((acc, guildCount) => acc + guildCount, 0)
         totalMembers = memberCount.reduce((acc, memberCount) => acc + memberCount, 0)
+        votes = await topApi.getVotes(client.user.id)
+
     } catch (error) {
         console.error(error)
     }
+
+    (await client.channels.fetch("1147811856749166703")).setName(`Servidores 🌐『 ${totalGuilds} 』`)
+
+    webhook.send({
+        embeds: [{
+            author: {
+                name: client.user.username,
+                icon_url: client.user.avatarURL({ forceStatic: false })
+            },
+            title: "Inicio / Reinicio de la bot iniciado",
+            color: configs.random_color(),
+            description: configs.code_text(`+ Status activa\n>> [${client.cmds.size}] comandos totales\n>> [${votes.length > 1? votes.length + '] votos': votes.length + '] voto'}\n>> [${totalGuilds}] Servidores\n>> [${totalMembers}] Usuarios`, 'diff')
+        }]
+    })
+
     return 0
 }
 
