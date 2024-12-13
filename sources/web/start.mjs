db1decfb3747aff4d2ec515f8230a0face7c3a69
CacaoNk0027 <jbzca@outlook.es>
2024-12-12 19:11:29 -0600

    configurando la web

--- sources/web/start.mjs
+++ sources/web/start.mjs
@@ -1,13 +1,29 @@
 import express from 'express'
+import cors from 'cors'
+import ejs from 'ejs'
+import router from './router/router.mjs'
+
+const app = express()
 
 function main() {
 
-    const app = express()
-    
     app.set('port', process.env.PORT || 449)
-    
-    app.get('/', (req, res) => {
-        res.send('hola mundo')
+
+    app.set('json spaces', 2)
+    app.set('views', 'sources/web/views')
+    app.set('view engine', 'ejs')
+
+    app.engine('html', ejs.renderFile)
+
+    app.use(express.urlencoded({ extended: false }))
+    app.use(express.json())
+    app.use(cors())
+
+    app.use(router)
+    app.use('/', express.static('sources/web/public'))
+
+    app.get('*', (req, res) => {
+        res.status(404).send('recurso no encontrado');
     })
 
     app.listen(app.get('port'), () => {
